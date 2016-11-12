081283b1a8e0e8d903250d1a9892b03f8d64b353
cbrenner04 <christopher.brenner1@northwestern.edu>
2016-11-12 17:41:25 -0600

    File Structure and Firebase

---
+++ assets/javascript/app.js
@@ -0,0 +1,9 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyAdGhATJcg2BS46FhM1GNsJaBrjfQ9Ew3Q",
+  authDomain: "rps-multiplayer-25dc4.firebaseapp.com",
+  databaseURL: "https://rps-multiplayer-25dc4.firebaseio.com",
+  storageBucket: "rps-multiplayer-25dc4.appspot.com",
+  messagingSenderId: "449061417401"
+};
+firebase.initializeApp(config);
