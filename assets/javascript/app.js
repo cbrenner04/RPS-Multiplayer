2be18896ddbca5cb822794bef9e49ada50272838
cbrenner04 <christopher.brenner1@northwestern.edu>
2016-11-21 09:53:01 -0600

    Notes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,5 +12,10 @@ firebase.initializeApp(config);
 var database = firebase.database();
 
 $(document).on('ready', function() {
-
+    // show entrance screen
+    // if current players are less than 2
+    // get name of current player
+    // show game board and messaging
+    // if only one current player, show a message about waiting for another player
+    // when both players have entered, allow game play
 });
