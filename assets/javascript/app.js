c2ef799a79aedaa37a42588fcd1a38bca7ee6288
cbrenner04 <christopher.brenner1@northwestern.edu>
2016-11-21 09:48:11 -0600

    Planning

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,16 @@
 // Initialize Firebase
 var config = {
-  apiKey: "AIzaSyAdGhATJcg2BS46FhM1GNsJaBrjfQ9Ew3Q",
-  authDomain: "rps-multiplayer-25dc4.firebaseapp.com",
-  databaseURL: "https://rps-multiplayer-25dc4.firebaseio.com",
-  storageBucket: "rps-multiplayer-25dc4.appspot.com",
-  messagingSenderId: "449061417401"
+    apiKey: "AIzaSyAdGhATJcg2BS46FhM1GNsJaBrjfQ9Ew3Q",
+    authDomain: "rps-multiplayer-25dc4.firebaseapp.com",
+    databaseURL: "https://rps-multiplayer-25dc4.firebaseio.com",
+    storageBucket: "rps-multiplayer-25dc4.appspot.com",
+    messagingSenderId: "449061417401"
 };
 firebase.initializeApp(config);
 
 // reference the database
 var database = firebase.database();
+
+$(document).on('ready', function() {
+
+});
