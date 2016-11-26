b8d35aa13742fdb735f5a082b1ce8e9cf0b8667c
cbrenner04 <christopher.brenner1@northwestern.edu>
2016-11-26 18:37:38 -0500

    Start game play functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,10 +10,17 @@ firebase.initializeApp(config);
 
 // reference the database
 var database = firebase.database();
+// global variable for currentPlayer
+var currentPlayer;
 
 $(document).on('ready', function() {
+    // listen for message removal
+    $('#remove-message').on('click', function() {
+        $('#welcome').hide();
+    });
+
     // if player already exists on this device set the username to currentPlayer
-    var currentPlayer = localStorage.getItem('username');
+    currentPlayer = localStorage.getItem('username');
     // if player does not already exist on this device
     if (currentPlayer === null) {
         // show the entrance screen and hide the gameplay screen to force username input
@@ -27,87 +34,196 @@ $(document).on('ready', function() {
             localStorage.setItem('username', currentPlayer);
             // set welcome message
             $('#welcome-message').text('Welcome ' + currentPlayer + '!');
-            // show welcome message
-            $('#welcome-message').show();
-            // hide the entrance screen
-            $('#entrance').hide();
-            // show the gameplay screen
-            $('#gameplay').show();
+            setUpGameBoard();
+            setUpGame();
             return false;
         });
         // if a player already exists on this device
     } else {
         // set the welcome message
         $('#welcome-message').text('Welcome back ' + currentPlayer + '!');
-        // set the username in the game board
-        $('#current-player').text(currentPlayer);
-        // show the welcome message
-        $('#welcome').show();
-        // hide the entrance screen
-        $('#entrance').hide();
-        // show the gameplay screen
-        $('#gameplay').show();
+        setUpGameBoard();
+        setUpGame();
     }
+});
 
-    $('#remove-message').on('click', function() {
-        $('#welcome').hide();
-    });
+function setUpGameBoard() {
+    // set the username in the game board
+    $('#current-player').text(currentPlayer);
+    // show the welcome message
+    $('#welcome').show();
+    // hide the entrance screen
+    $('#entrance').hide();
+    // show the gameplay screen
+    $('#gameplay').show();
+}
 
+var gameKey;
+var playerOne;
+var playerTwo;
+var currentPlayerWins;
+var otherPlayerWins;
+
+function setUpGame() {
     // get most recent game object
-    database.ref().orderByChild('timestamp').limitToLast(1).once('value', function(snapshot) {
-        var key = Object.keys(snapshot.val()).toString();
-        var playerTwo = snapshot.val()[key].player2;
+    database.ref().orderByChild('timestamp').limitToLast(1).on('value', function(snapshot) {
+        gameKey = Object.keys(snapshot.val()).toString();
+        playerOne = snapshot.val()[gameKey].player1;
+        playerTwo = snapshot.val()[gameKey].player2;
+
+        if (currentPlayer === playerOne) {
+            currentPlayerWins = snapshot.val()[gameKey].playerOneWins;
+            otherPlayerWins = snapshot.val()[gameKey].playerTwoWins;
+            displayWins();
+        } else if (currentPlayer === playerTwo) {
+            currentPlayerWins = snapshot.val()[gameKey].playerTwoWins;
+            otherPlayerWins = snapshot.val()[gameKey].playerOneWins;
+            displayWins();
+        }
 
         // check for current "open" game
         // if there is an open game
         if (playerTwo === undefined) {
-            // the current player is player 2
-            database.ref().child(key).update({
-                player2: currentPlayer
-            });
-        } else {
-            // create new game
-            // the current player is player 1
-            database.ref().push({
-                timestamp: firebase.database.ServerValue.TIMESTAMP,
-                player1: currentPlayer
-            });
+            // check that current player is not already attached to this game
+            if (playerOne !== currentPlayer) {
+                // the current player is player 2
+                database.ref().child(gameKey).update({
+                    player2: currentPlayer,
+                    playerTwoWins: 0
+                });
+            }
+        // check that current player is not already attached to latest game
+        } else if (playerOne !== currentPlayer && playerTwo !== currentPlayer) {
+            createNewGame();
+        } else if (playerOne === currentPlayer) {
+            // set player2 as the other player
+            $('#other-player').text(playerTwo);
+            // hide waiting alert
+            $('#waiting-on-player').hide();
+            // show other player card
+            $('#other-player-panel').show();
+            // play the game
+            playGame();
+        } else if (playerTwo === currentPlayer) {
+            // set player1 as the other player
+            $('#other-player').text(playerOne);
+            // hide waiting alert
+            $('#waiting-on-player').hide();
+            // show other player card
+            $('#other-player-panel').show();
+            // play game
+            playGame();
         }
     });
+}
 
-    /*
-        show game board and messaging
-            game board should show user name, wins, and losses
-            data will come from firebase
-        -- check for sessionStorage for current players --
-        if current players are less than 2
-            show a message about waiting for another player
-            do not allow game play
-        else
-            allow game play
-     */
+function displayWins() {
+    $('#current-wins').text(currentPlayerWins || 0);
+    $('#current-losses').text(otherPlayerWins || 0);
+    $('#other-wins').text(otherPlayerWins);
+    $('#other-losses').text(currentPlayerWins);
+}
 
-    /*
-        listen for player choice
-        if both players have chosen
-            display the result
-            log win to winner on firebase
-            log loss to loser on firebase
-        else if current player has chosen
-            display message that current player needs to choose
-        else if other play has not chosen
-            display message that waiting on other players choice
+function createNewGame() {
+    // create new game
+    // the current player is player 1
+    database.ref().push({
+        timestamp: firebase.database.ServerValue.TIMESTAMP,
+        player1: currentPlayer,
+        playerOneWins: 0
+    }).push({
+        // initialize first round
+        timestamp: firebase.database.ServerValue.TIMESTAMP
+    });
+    // show waiting on player alert
+    $('#waiting-on-player').show();
+    // hide player 2 card
+    $('#other-player-panel').hide();
+    setUpGame();
+}
 
-        after round is complete
-            reset currentPlayers
-            hide gameplay
-            show restart
-     */
+function playGame() {
+    $('#new-game').on('click', function() {
+        createNewGame();
+    });
 
+    var roundKey;
+    var playerOneChoice;
+    var playerTwoChoice;
+
+    database.ref().child(gameKey).orderByChild('timestamp').limitToLast(1).on('value', function(snapshot) {
+        roundKey = Object.keys(snapshot.val()).toString();
+        playerOneChoice = snapshot.val()[roundKey].player1;
+        playerTwoChoice = snapshot.val()[roundKey].player2;
+
+        if (playerOneChoice !== undefined && playerTwoChoice !== undefined) {
+            if (playerOne === currentPlayer) {
+                $('#other-choice').text('They played ' + playerTwoChoice);
+            } else if (playerTwo === currentPlayer) {
+                $('#other-choice').text('They played ' + playerOneChoice);
+            }
+
+            if (playerOneChoice === playerTwoChoice) {
+                $('#current-result').text('You tied');
+                $('#other-result').text('They tied');
+            } else if ((playerOneChoice === 'Rock' && playerTwoChoice === 'Scissors') ||
+                      (playerOneChoice === 'Scissors' && playerTwoChoice === 'Paper') ||
+                      (playerOneChoice === 'Paper' && playerTwoChoice === 'Rock')) {
+                if (currentPlayer === playerOne) {
+                    $('#current-result').text('You win!');
+                    $('#other-result').text('They lose!');
+                } else {
+                    $('#current-result').text('You lose!');
+                    $('#other-result').text('They win!');
+                }
+                // database.ref().child(gameKey).update({
+                //     playerOneWins: currentPlayerWins + 1
+                // });
+            } else {
+                if (currentPlayer === playerTwo) {
+                    $('#current-result').text('You win!');
+                    $('#other-result').text('They lose!');
+                } else {
+                    $('#current-result').text('You lose!');
+                    $('#other-result').text('They win!');
+                }
+                // database.ref().child(gameKey).update({
+                //     playerTwoWins: currentPlayerWins + 1
+                // });
+            }
+        }
+
+        $('.choice').on('click', function() {
+            var choice = $(this).text();
+            $('#current-choice').text('You chose ' + choice);
+            if (playerOne === currentPlayer && playerOneChoice === undefined) {
+                database.ref().child(gameKey).child(roundKey).update({
+                    player1: choice
+                });
+            } else if (playerTwo === currentPlayer && playerTwoChoice === undefined) {
+                database.ref().child(gameKey).child(roundKey).update({
+                    player2: choice
+                });
+            }
+
+            return false;
+        });
+
+        if (playerOne === currentPlayer) {
+            if (playerTwoChoice === undefined) {
+                $('#other-choice').text('Waiting on other player to choose');
+            }
+
+        } else if (playerTwo === currentPlayer) {
+            if (playerOneChoice === undefined) {
+                $('#other-choice').text('Waiting on other player to choose');
+            }
+        }
+    });
+}
     /*
         clear message log each new session
             -- variable in sessionStorage? --
         listen for player message
         display user name and message
      */
-});
